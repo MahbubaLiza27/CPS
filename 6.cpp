a5bb45b2c9a795fb225e41e0ed6dfb994d75a426
MahbubaLiza27 <118561598+MahbubaLiza27@users.noreply.github.com>
2022-11-19 00:07:26 -0800

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,21 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+   float a,b,c;
+   cin>>a>>b>>c;
+   cout<<"MEDIA = " ;
+   cout<< fixed << setprecision(1) << (a+b+c)/3 << endl;
+
+
+}
+
+
+
+
+
+
+
