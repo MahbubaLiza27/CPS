4d904f54d81bd5b0396ab389d4ccb49306c799b8
MahbubaLiza27 <118561598+MahbubaLiza27@users.noreply.github.com>
2022-11-18 23:45:14 -0800

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+
+    float avg;
+    float sum;
+    float a, b;
+
+    cin >> a >> b;
+    cout << "MEDIA = ";
+    sum = a + b;
+
+    avg = sum / 2;
+
+    cout << fixed << setprecision(5) << avg << endl;
+
+}
+
+
+
+
+
+
