3672ef8b1a2c5c87b3fa0625504d4148809bec07
MahbubaLiza27 <118561598+MahbubaLiza27@users.noreply.github.com>
2022-11-18 23:52:54 -0800

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,21 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+   float r, pi=3.14159;
+   cin >> r;
+   //float A= pi * r *r;
+   cout << "A=" ;
+   cout<< fixed << setprecision(4) << pi*r*r << endl;
+
+
+}
+
+
+
+
+
+
